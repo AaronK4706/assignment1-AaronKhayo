d15c065c5ba08d4ad8e94e62e4d4b37c84442b6e
AaronK4706 <aaron.khayo@msj.edu>
2026-04-20 09:24:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
 const button = document.getElementById("themeBtn");
 
-
-button.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-});
+if (button) {
+    button.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
+}
